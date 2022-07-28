f35f5f7f45bedc8acd23c7186132bf2fbfdd5570
Daruuu <thedaruny16@gmail.com>
2022-07-29 00:58:02 +0200

    add exercise 14 js v3

--- 6.JS-jQUERY/exerciseJS14/main.js
+++ 6.JS-jQUERY/exerciseJS14/main.js
@@ -1,60 +1,74 @@
 /*
 1. a partir de la pizza subtituye todos los elementos del array por cervezas.
 */
-const foodEmojis = ["🍔","🍱","🌯","🍣","🫕","🍕","🍙","🥩","🫓"];
+const foodEmojis = ["🍔", "🍱", "🌯", "🍣", "🍕", "🫕", "🍙", "🥩", "🫓"]; //9
 
+const emojisForEach = foodEmojis.forEach(element => {
+    console.log(element);
+});
 
-//const emojisForEach = foodEmojis.forEach(element => {
-//    console.log(element);
-//});
-/*
-function replaceArrayPosition() {
-
-    for (let i = 0; i < foodEmojis.length; i++) {
-        const newVar = foodEmojis.indexOf("🍕");
-    
-        if ( newVar === "🍕"){
-            foodEmojis[i].replace("🍺");
-        }
-    console.log(foodEmojis);
-    }
-}
-replaceArrayPosition();
-*/
-function replaceArrayItem() {
-    for (let i = 0; i < foodEmojis.length; i++) {
-        const element = foodEmojis[i];
-        
-        if (element === "🍕") {
-            element.replaceArrayItem("🍺");
-        }
-    }
-    
-}
 
 
 
 //2. encontrar si existe un elemento en el array que sea un pina.
-
-const arrayPinas = ["🍕","🍕","🍍","🍕","🍕",];
-
 //3. quita la pina del array
 
-arrayPinas.reduce(2,"🍍");
-console.log(arrayPinas);
+const arrayPinas = ["🍕", "🍕", "🍍", "🍕", "🍕"]; //4
+console.log("original array: " + arrayPinas);
+
+function removeItemPina(arrayPinas, pina) {
+    let i = arrayPinas.indexOf(pina);
+    if (i >= 0) {
+        arrayPinas.splice(i, 1);
+    }
+    console.log("array update: " + arrayPinas);
+};
+removeItemPina(arrayPinas, "🍍");
+
+
 
 //4. convierte todas las fresas en 🍄
 
-const arrayLimon = ["🍓","🍋","🍓","🍋","🍓"];
+const arrLimon = ["🍓", "🍋", "🍓", "🍋", "🍓"];
+
+for (let i = 0; i < arrLimon.length; i++) {
+    if (arrLimon[i] === "🍓") {
+        arrLimon[i] = "🍄";
+    }
+}
+console.log(arrLimon);
 
 
 
-/*
-*/
+
+//5. añade un icono 👽 despues de cada 🌶️:
+
+const arrayAlien = ["🌶️","🥛","🌶️","🥛","🌶️","🥛"];
+//const alien = ["👽"];
+//const anadirItem = arrayAlien.map(e => "🌶️" + "👽");
+
+//const updateAlien = arrayAlien.splice("🌶️",0,"👽");
+
+function newArray() {
+    for (let i = 0; i < arrayAlien.length; i++) {
+        while (arrayAlien[i] === "🌶️") {
+            arrayAlien == arrayAlien.splice(i+1,0,"👽");
+            console.log(arrayAlien);
+            break;
+        }
+    }
+}
+newArray();
+console.log(arrayAlien);
+//console.log(anadirItem);
+
+// añade un 🃏 entre medio de 2 cartas:
+
+const arrComodin = [""];
 
 
 
 
 
-/*
-*/
+
+
