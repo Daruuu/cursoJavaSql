008f71444365c8508d851fed9a7c2e1bce9caf3b
Daruuu <thedaruny16@gmail.com>
2022-07-31 17:20:22 +0200

    adding  exer 15-v2

--- 6.JS-jQUERY/exerciseJS15/main.js
+++ 6.JS-jQUERY/exerciseJS15/main.js
@@ -1,10 +1,42 @@
 $(document).ready(function () {
-    //alert("this works");
-
-    
+    //alert("this works");    
 });
 
+const arraylive = ["🤖","👽","🧙‍♂️","💪","👾","🔒","👍"];
+
+// ADD ITEM
+//whe click on push add elemnt to array
+function push() {
+    alert("push funciona");
+}
+
+function unshift() {
+    alert("unshift funciona");
+    
+}
+
+function insertAt() {
+    alert("insertAt funciona");
+    
+}
+
+//REMOVE ITEM
+function pop() {
+    alert("pop funciona");
+}
+
+function shift() {
+    alert("shift funciona");
+    
+}
+
+function removeAt() {
+    alert("removeAt funciona");
+    
+}
 
 
-
-
+function arrayUI() {
+    const arrayEmpty = [];
+    
+}
---
+++ 6.JS-jQUERY/exerciseJS16/main.js
@@ -0,0 +1,18 @@
+//1. mediante a jQuery haz un boton que mediante a un toggle que cambie el texto de un parrafo al color rojo
+
+$().ready(() => {
+    //alert("this works");
+    $("#btn1").click( function () {
+        $("p1").css('color', 'red');
+    });
+});
+
+
+
+
+
+
+
+
+
+//2. mediante al JS haz otro boton que mediante a un toggle el color del parrafo azul
