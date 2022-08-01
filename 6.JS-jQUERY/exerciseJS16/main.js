1a6aa1684ac3c1ebe076c5c2a1c4d40c6f9850d4
Daruuu <thedaruny16@gmail.com>
2022-08-01 16:30:44 +0200

    adding exercise 16 v1

--- 6.JS-jQUERY/exerciseJS16/main.js
+++ 6.JS-jQUERY/exerciseJS16/main.js
@@ -1,20 +1,17 @@
 //1. mediante a jQuery haz un boton que mediante a un toggle que cambie el texto de un parrafo al color rojo
 
-//const p1 = document.getElementById("p1");
-
 $().ready(() => {
     //alert("this works");
-    $('#btn1').click(function(){
+    $('#btn1').click(function () {
         $("#p1").css('color', 'green');
     });
 });
 
 
-
-
-
-
-
-
-
 //2. mediante al JS haz otro boton que mediante a un toggle el color del parrafo azul
+
+$().ready(() => {
+    $('#btn2').on("click", function () {
+        $('#p2').css('color', 'blue');
+    });
+});
