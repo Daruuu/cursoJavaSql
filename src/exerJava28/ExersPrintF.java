89ad65652be01ca245db4ee79aa3db0111cc7675
Daruuu <thedaruny16@gmail.com>
2022-08-11 17:04:19 +0200

    automate-commit with makefile

---
+++ src/exerJava28/ExersPrintF.java
@@ -0,0 +1,13 @@
+package exerJava28;
+
+public class ExersPrintF {
+    public static void main(String[] args) {
+        ExersPrintF ef = new ExersPrintF();
+        ef.exerciseOne();
+    }
+
+    public void exerciseOne(){
+        System.out.printf("%s%n", "Hola"); //Hola
+
+    }
+}
