373008bc8374093c1ffc0d579dc0b8665f3eb861
Daruuu <thedaruny16@gmail.com>
2022-08-13 00:41:16 +0200

    update 2 script bash

--- gitpush.sh
+++ gitpush.sh
@@ -4,8 +4,9 @@ git add .
 	echo "Enter the commit message:"
 read COMMITMESSAGE
 git commit -m "$COMMITMESSAGE"
-	echo  "enter the name of branch:"
+	echo  "enter the name of branch (main):"
 read BRANCH
 git push origin $BRANCH
 read
 	echo "REPOSITORY UPDATED SUCCESFUL!!"
+git log -n3
